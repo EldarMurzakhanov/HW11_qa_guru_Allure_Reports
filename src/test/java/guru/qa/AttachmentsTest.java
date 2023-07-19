bdc5a4d3a409bbbf8468d6c3b017e60e7fedf8d6
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-07-19 04:41:49 +0300

    init commit

---
+++ src/test/java/guru/qa/AttachmentsTest.java
@@ -0,0 +1,42 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static io.qameta.allure.Allure.attachment;
+import static org.openqa.selenium.By.linkText;
+
+
+public class AttachmentsTest {
+
+    private static final String REPOSITORY = "EldarMurzakhanov/HW11_qa_guru_Allure_Reports";
+    private static final int ISSUE = 1;
+
+    @Test
+    public void testLambdaAttachments() throws InterruptedException {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotadedAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.takeScreenshot();
+
+
+    }
+
+}
+
--- src/test/java/guru/qa/WebSteps.java
+++ src/test/java/guru/qa/WebSteps.java
@@ -1,7 +1,12 @@
 package guru.qa;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -35,5 +40,9 @@ public class WebSteps {
     @Step("Проверяем наличие Issue c номером {issue}")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+        @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
